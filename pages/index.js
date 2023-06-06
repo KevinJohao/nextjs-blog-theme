504d043337b1c0e3ad89e62737504aad8df33746
KevinJohao <57634076+KevinJohao@users.noreply.github.com>
2023-06-06 09:54:57 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,10 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
+<h1 className="text-3xl lg:text-5xl text-center mb-12">
+          Holaaaaa
+        </h1>
+
         <ul className="w-full">
           {posts.map((post) => (
             <li
